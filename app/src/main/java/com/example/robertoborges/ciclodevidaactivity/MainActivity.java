ddcaed687f9fe6ad1d9f1fe62071a2235d8b36e9
Roberto Borges <robertoborges_jimez@hotmail.com>
2017-01-21 00:43:10 -0430

    Ciclo De Vida Activity v 1.0

---
+++ app/src/main/java/com/example/robertoborges/ciclodevidaactivity/MainActivity.java
@@ -0,0 +1,52 @@
+package com.example.robertoborges.ciclodevidaactivity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toast.makeText(this,R.string.onCreate,Toast.LENGTH_SHORT).show();
+    }
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Toast.makeText(this,R.string.onStart,Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Toast.makeText(this,R.string.onResume,Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Toast.makeText(this,R.string.onPause,Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        Toast.makeText(this,R.string.onRestart,Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Toast.makeText(this,R.string.onStop,Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Toast.makeText(this,R.string.onDestroy,Toast.LENGTH_SHORT).show();
+    }
+
+}
+
